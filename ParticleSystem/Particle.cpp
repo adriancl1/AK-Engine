296a32dadec52f816b5cf5f2e50a5f54123052a5
marcsamper <msamperpalafolls@gmail.com>
2017-12-12 21:11:24 +0100

    Started Implementing Particle Class

--- ParticleSystem/Particle.cpp
+++ ParticleSystem/Particle.cpp
@@ -2,10 +2,62 @@
 #include "ParticleSystem.h"
 
 
-Particle::Particle()
+
+Particle::Particle(ParticleSystem * parent, const SystemState & initial, const SystemState & final, float3 Speed, float MaxLifeTime)
 {
+
+	Data.MaxLifeTime = MaxLifeTime;
+	Data.Speed = Speed;
 }
+
 
 Particle::~Particle()
 {
+}
+
+bool Particle::PreUpdate(float dt)
+{
+	//billboard
+
+	float3 Orientation = pSystem->cameraPos - Data.Position;
+	Orientation.y = Data.Position.y;
+	Data.Rotation= Quat::LookAt(float3(0.0f, 0.0f, 1.0f), Orientation, float3(0.0f, 1.0f, 0.0f), float3(0.0f, 1.0f, 0.0f));
+	return true;
+}
+
+bool Particle::Update(float dt) 
+{
+	Data.LifeTime += dt;
+	CalcInterpolation();
+	return true;
+}
+
+bool Particle::PostUpdate(float dt) 
+{
+	if (Data.LifeTime >= Data.MaxLifeTime) 
+	{
+
+	}
+}
+
+void Particle::CalcInterpolation() 
+{
+	//Position
+	Data.Speed += Data.force*Data.LifeTime;
+	Data.Position += Data.Speed*Data.LifeTime;
+
+	//Size;
+	Data.Size = Initial.Size + Data.LifeTime*(Final.Size - Initial.Size);
+
+	//Color
+	Data.RGBA.x = Initial.RGBA.x + Data.LifeTime*(Final.RGBA.x - Initial.RGBA.x);
+	Data.RGBA.y = Initial.RGBA.y + Data.LifeTime*(Final.RGBA.y - Initial.RGBA.y);
+	Data.RGBA.z = Initial.RGBA.z + Data.LifeTime*(Final.RGBA.z - Initial.RGBA.z);
+	Data.RGBA.w = Initial.RGBA.w + Data.LifeTime*(Final.RGBA.w - Initial.RGBA.w);
+
+	//Gravity
+
+	Data.force.x = Initial.force.x + Data.LifeTime*(Final.force.x - Initial.force.x);
+	Data.force.y = Initial.force.y + Data.LifeTime*(Final.force.y - Initial.force.y);
+	Data.force.z = Initial.force.z + Data.LifeTime*(Final.force.z - Initial.force.z);
 }
--- ParticleSystem/Particle.h
+++ ParticleSystem/Particle.h
@@ -1,21 +1,57 @@
 #ifndef _PS_PARTICLE_
 #define _PS_PARTICLE_
 
+#include "..\MathGeo\Math\float3.h"
+#include "..\MathGeo\Math\float4.h"
+#include "..\MathGeo\Math\Quat.h"
+
+struct ParticleData
+{
+	float3 Position = float3::zero;					//Particle Actual Position
+	Quat Rotation = Quat::identity;					//Particle Actual Rotation
+	float3 Scale = float3::one;						//Particle Actual Scale
+	float Size = 0.0f;								//Scale Multiplicator to modify
+	float3 Speed = float3::zero;					//Particle Speed
+	float3 Direction = float3::zero;		//Particle Emission Direction
+	float3 force = float3::zero;					//Force that effects that particle
+	float MaxLifeTime = 0;							//Max Particle Lifetime
+	float LifeTime = 0;								//Actual Particle Lifetime
+	uint TextureID = 0;								//Texture ID used by this particle
+	float4 RGBA = float4::zero;
+};
+
+struct State 
+{
+	float3 force = float3::zero;					//Gravity that affects that particle
+	float Size = 0.0f;
+	float4 RGBA = float4::one;
+};
+
 class ParticleSystem;
 
 class Particle
 {
 public:
-	Particle();
+
+	Particle(ParticleSystem* parent, const SystemState& initial , const SystemState& final, float3 Speed, float MaxLifeTime);
 	~Particle();
 
+	bool PreUpdate(float dt);
+	bool Update(float dt);
+	bool PostUpdate(float dt);
 
+	void CalcInterpolation();
 
 
 private:
 	ParticleSystem * pSystem;
+	ParticleData Data;
+	State Initial;
+	State Final;
 
 };
+
+
 
 
 
